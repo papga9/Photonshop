4b9381f61021560156de2282abe478d1b1e1bcff
Papp Gábor Richárd <39255674+papga9@users.noreply.github.com>
2024-11-05 18:16:37 +0100

    Init

--- Photonshop WPF/MainWindow.xaml.cs
+++ Photonshop WPF/MainWindow.xaml.cs
@@ -10,15 +10,24 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Photonshop_WPF
+
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        BitmapImage bm = new BitmapImage();
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Button_click(object sender, RoutedEventArgs e)
+        {
+            bm.BeginInit();
+            bm.UriSource = new Uri("C:\\Users\\pappg\\my stuff\\Code Repos\\Photonshop\\Photonshop WPF\\bin\\Debug\\net8.0-windows\\shrek.jpg");
+            bm.EndInit();
+            imageViewer.Source = bm;
         }
     }
 }
